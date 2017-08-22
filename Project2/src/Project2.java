8f41762f2bd49838744a31fedfa41616d053019b
kiefferladd <kiefferxladd@gmail.com>
2017-08-22 09:29:51 -0700

    Kieffer Manalang

---
+++ Project2/src/Project2.java
@@ -0,0 +1,12 @@
+
+//Kieffer Manalang
+// My second javav project. It will compare and printIn.
+// August 22, 2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello -- this is project 2!!!");
+	}
+
+}
