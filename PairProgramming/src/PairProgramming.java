68929c6ed025e655b61c96d0361125291aad9387
kiefferladd <kiefferxladd@gmail.com>
2017-10-03 09:24:31 -0700

    Pair Programming

---
+++ PairProgramming/src/PairProgramming.java
@@ -0,0 +1,20 @@
+
+public class PairProgramming {
+
+	public static void main(String[] args) {
+		Scanner1 = new 
+				scanner(system.in);
+				boolean done = false;
+		int sum = 0;
+		while (!done) {
+			System.out.println ("Type a number");
+			int n = scanner1.nextint();
+			sum += n;
+			System.out.println("areyoudone?");
+			String finished = scanner1.next();
+			if (finished.equals("yes")) {
+				done = true;
+			}
+		}
+	}
+}
